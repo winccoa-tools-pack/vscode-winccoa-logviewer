244f41ff3a256d7c7d2e5ede1545b795d80a7cbc
Martin Pokorny <martin.pokorny@etm.at>
2026-03-25 21:46:28 +0100

    chore: update ESLint TODO link to correct repo issue #33

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -51,7 +51,7 @@ module.exports = tseslint.config(
       // Mirror your original TS rule settings
       '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
       '@typescript-eslint/no-explicit-any': 'off',
-      // TODO: Fix these issues - see https://github.com/winccoa-tools-pack/vscode-winccoa-ctrllang/issues/33
+      // TODO: Fix these issues - see https://github.com/winccoa-tools-pack/vscode-winccoa-logviewer/issues/33
       '@typescript-eslint/no-require-imports': 'off',
       'no-useless-escape': 'off',
     },
